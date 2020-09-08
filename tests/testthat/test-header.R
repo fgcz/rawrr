f2883c4da0bee2b2b97e6268e73342483f0f1adb
Christian Panse <cpanse@users.noreply.github.com>
2020-09-08 22:51:45 +0200

    Update test-header.R
    cleanup

--- tests/testthat/test-header.R
+++ tests/testthat/test-header.R
@@ -18,18 +18,4 @@ test_that("check readFileHeader", {
   expect_equal(M$`Scan range`, c(1, 574))
 })
 
-test_that("check readSpectrum 23.", {
-
-  rawfile <- file.path(path.package(package = 'rawR'), 'extdata', 'sample.raw')
-  S <- readSpectrum(rawfile, 23)[[1]]
-
-  DF <- read.table(file.path(path.package(package = 'rawR'), 'extdata', 'scan23_peakList.txt'), sep="\t", header=TRUE)
-
-  expect_true(sum(S$mZ %in% DF$m.z) >= 720)
-  expect_true(sum(S$intensity %in% DF$Intensity) >= 720)
-
-  lapply(DF$m.z[DF$Flags == "F"] %in% S$mZ, expect_true)
-  lapply(DF$m.z[DF$Flags == "M"] %in% S$mZ, expect_true)
-  lapply(DF$m.z[DF$Flags == "E"] %in% S$mZ, expect_false)
-})
 
