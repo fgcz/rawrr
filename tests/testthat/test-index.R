ab3a45c42fb602d83db0c186fcf6c0856c77b07d
Christian Panse <cp@fgcz.ethz.ch>
2020-12-09 17:18:57 +0100

    added test cases for readIndex

--- tests/testthat/test-index.R
+++ tests/testthat/test-index.R
@@ -6,11 +6,16 @@ library(rawrr)
 
 
 test_that("check readIndex.", {
-
-  S <- readIndex(sampleFilePath())
-
-  expect_equivalent(dim(S) == c(573, 6),
+  x <- readIndex(sampleFilePath())
+  expect_equivalent(dim(x) == c(573, 8),
       c(TRUE, TRUE))
+  expect_true(is.data.frame(x))
+  IndexColNames <- c("scan", "scanType", "rtinseconds", "precursorMass",
+                     "MSOrder", "charge", "masterScan", "dependencyType")
+  expect_true(is.integer(x$scan))
+  expect_true(all(IndexColNames %in% colnames(x)))
+  expect_true(is.logical(x$dependencyType))
+  expect_true(all(1:nrow(x), x$scan))
 })
 
 
