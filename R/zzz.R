df742bd923d2b25c5d2e1905c1c05c2b5aa7df6d
Christian Panse <cp@fgcz.ethz.ch>
2020-12-10 23:09:42 +0100

    #26 copyright as UTF-8 Encoding.

--- R/zzz.R
+++ R/zzz.R
@@ -9,7 +9,7 @@
 
 	if(interactive()){
 		version <- packageVersion('rawrr')
-		thermocopyright <- "RawFileReader reading tool. Copyright © 2016 by Thermo Fisher Scientific, Inc. All rights reserved."
+		thermocopyright <- "RawFileReader reading tool. Copyright \u00A9 2016 by Thermo Fisher Scientific, Inc. All rights reserved."
 		packageStartupMessage("Package 'rawrr' version ", version, " using\n", thermocopyright)
 	  invisible()
         }
