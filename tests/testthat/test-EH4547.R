77e7436cac3ef21e5ebd66dc7957e1e0b4ba35ac
Christian Panse <cp@fgcz.ethz.ch>
2021-03-05 11:22:09 +0100

    test case for tic of EH4547

---
+++ tests/testthat/test-EH4547.R
@@ -0,0 +1,24 @@
+#R
+
+context("EH4547")
+
+library(rawrr)
+library(ExperimentHub)
+
+
+test_that("check tic of EH4547", {
+  eh <- ExperimentHub::ExperimentHub()
+  EH4547 <- normalizePath(eh[["EH4547"]])
+  rawfile <- paste0(EH4547, ".raw")
+  if (!file.exists(rawfile)){ file.link(EH4547, rawfile) }
+
+  expect_true(file.exists(EH4547))
+  expect_true(file.exists(rawfile))
+
+  x <- rawrr::readChromatogram(rawfile = rawfile, type = "tic")
+
+  expect_equal(length(x$times), length(x$intensities))
+  expect_equal(length(x$intensities), 995)
+
+  expect_true(sum(x$intensities) == 63682815178)
+})
