5583e3bb4f94ac9e3be510451906b4f96fea3f7c
Christian Panse <cp@fgcz.ethz.ch>
2021-06-05 15:20:40 +0200

    update rawrr.exe assembly

--- R/dotNetAssembly.R
+++ R/dotNetAssembly.R
@@ -232,7 +232,7 @@ installRawFileReaderDLLs <-
 #' @aliases rawrr.exe
 #' @export installRawrrExe
 installRawrrExe <-
-  function (sourceUrl = "http://fgcz-ms.uzh.ch/~cpanse/rawrr/rawrr.0.99.26.exe",
+  function (sourceUrl = "http://fgcz-ms.uzh.ch/~cpanse/rawrr/rawrr.1.1.5.exe",
             ...)
   {
    
