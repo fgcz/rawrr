b57c307477c0db7c2544ecf9f35fa812c667b635
Christian Panse <cp@fgcz.ethz.ch>
2024-08-23 11:29:15 +0200

    add .computeMassShift function

--- R/rawrr.R
+++ R/rawrr.R
@@ -1440,6 +1440,61 @@ dependentScan <- function(x, scanNumber){
     return(i)
 }
 
+## compute mass shift of a given file and mass tag
+.computeMassShift <- function(f, mass = c(758.5694, 760.5851)){
+  message("reading xic from file ", basename(f), " ...")
+  xic_ <- rawrr::readChromatogram(f, mass = mass)
+  
+  message("reading index from file ", basename(f), "...")
+  Idx <- rawrr::readIndex(f)
+  Idx.Ms <- Idx[which(Idx$MSOrder=="Ms"), ]
+  
+  lapply(1:length(mass), function(ii){
+    xic <- xic_[[ii]]
+    idx <- which(xic$intensities == max(xic$intensities))
+    if (length(idx) != 1) {
+      warning("more than one hit for ", mass[ii], " in ", basename(f))
+      return(NULL)
+    }
+    ## pick APEX and 5 left and right
+    idxs <- seq(idx - 5, idx + 5)
+    
+    message("merging xic and index data.frames...")
+    data.frame(StartTime=xic$times[idxs]) |> merge(Idx.Ms) -> qMs
+    if (nrow(qMs) == 0) {
+      warning("nothing to merge ", mass[ii], " in ", f)
+      return(NULL)
+    }
+    
+    message("reading ", nrow(qMs) , " spectra from file ", basename(f), "...")
+    
+    ## find nearest peak
+    rawrr::readSpectrum(f, scan = qMs$scan) |>
+      sapply(FUN = function(x){
+        protViz::findNN(mass[ii], x$centroid.mZ) -> hit;
+        x$centroid.mZ[hit]
+      }) -> mzDiff
+    
+    ## compose output in long format
+    rbind(
+      data.frame(
+        times = xic$times[idxs],
+        value = xic$intensities[idxs],
+        attribute = "intensities",
+        file = basename(f),
+        mass = mass[ii]
+      ),
+      data.frame(
+        times = xic$times[idxs],
+        value = (mzDiff - mass[ii]),
+        attribute = "mzDiff",
+        file = basename(f),
+        mass = mass[ii]
+      ))
+  }) |>
+    Reduce(f = rbind) 
+}
+
 # returns a sequence of indicies representing one peak area
 .extractMaximumPeak <- function(y){
   # 1st max peak
