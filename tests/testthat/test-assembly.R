32f6ad542dcb371c8f13de7c4cdd3a2b6597b885
Christian Panse <cp@fgcz.ethz.ch>
2025-06-07 12:33:23 +0200

    add test case for assembly version

--- tests/testthat/test-assembly.R
+++ tests/testthat/test-assembly.R
@@ -2,7 +2,14 @@
 
 context(".Net assembly")
 
-test_that("check tic of EH4547", {
+test_that("check assembly file path", {
   expect_true(file.exists(rawrr:::.rawrrAssembly()))
+})
+
+test_that("check assembly execute", {
   expect_true(.isAssemblyWorking())
+})
+
+test_that("check assembly version", {
+  expect_true(rawrr:::.getRawrrAssemblyVersion() > "1.17.1")
 })
