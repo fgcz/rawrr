e0b3e2b31b226415a30d551f9af53b3ec1f5e11e
Christian Panse <cp@fgcz.ethz.ch>
2025-06-10 08:58:21 +0200

    add rawrr assembly version test in testthat.R
    - ensure that `R CMD check` uses current rawrr assembly
    - version bump to 1.17.6

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,4 +4,8 @@ library(testthat)
 
 suppressPackageStartupMessages(library(rawrr))
 
+if (rawrr:::.getRawrrAssemblyVersion() < "1.17.2"){
+  rawrr::installRawrrExe(force = TRUE)
+}
+
 test_check("rawrr")
